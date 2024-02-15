7b1c25226eed089a562c18191bf4426af57d9413
PengChaoQun <1152684231@qq.com>
2024-02-15 19:59:54 +0800

    ✨ feat: 增加路径别名；增加tsx配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: PengChaoQun 1152684231@qq.com
  * @Date: 2024-01-29 20:15:38
  * @LastEditors: PengChaoQun 1152684231@qq.com
- * @LastEditTime: 2024-02-07 20:55:57
+ * @LastEditTime: 2024-02-15 10:31:34
  * @FilePath: /experience-book-vue3/vite.config.ts
  * @Description:
  */
@@ -13,14 +13,16 @@ import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 import * as path from 'path';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   mode: 'production',
-  
+
   plugins: [
     vue(),
+    vueJsx(),
     Components({
       resolvers: [
         AntDesignVueResolver({
@@ -48,6 +50,10 @@ export default defineConfig({
       {
         find: '@layout',
         replacement: path.resolve('./src/layout/')
+      },
+      {
+        find: '@views',
+        replacement: path.resolve('./src/views/')
       }
     ]
   }
