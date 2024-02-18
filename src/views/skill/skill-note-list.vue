2b3e9ba5e0c18241e9b6f46d6278067bc3c63e75
PengChaoQun <1152684231@qq.com>
2024-02-18 22:05:08 +0800

    ✨ feat: 优化样式

--- src/views/skill/skill-note-list.vue
+++ src/views/skill/skill-note-list.vue
@@ -2,7 +2,7 @@
  * @Author: PengChaoQun 1152684231@qq.com
  * @Date: 2024-02-02 10:52:27
  * @LastEditors: PengChaoQun 1152684231@qq.com
- * @LastEditTime: 2024-02-18 21:56:54
+ * @LastEditTime: 2024-02-18 22:03:04
  * @FilePath: /experience-book-vue3/src/views/skill/skill-note-list.vue
  * @Description: 
 -->
@@ -92,7 +92,7 @@
     </div>
 
     <!-- 编辑器 -->
-    <div v-if="activeNote.id" class="note-editor flex flex-col overflow-y-auto bg-white">
+    <div v-if="activeNote.id" class="note-editor flex flex-col overflow-y-auto bg-white w-full overflow-x-hidden">
       <a-form
         :model="form"
         ref="formRef"
