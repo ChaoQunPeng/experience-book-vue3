3c7bdb8f93531c93623738ea49cb1e206b0a7f6f
PengChaoQun <1152684231@qq.com>
2024-02-19 11:23:08 +0800

    ✨ feat: 调整github pages配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@
  * @Author: PengChaoQun 1152684231@qq.com
  * @Date: 2024-01-29 20:15:38
  * @LastEditors: PengChaoQun 1152684231@qq.com
- * @LastEditTime: 2024-02-15 10:31:34
+ * @LastEditTime: 2024-02-19 11:21:48
  * @FilePath: /experience-book-vue3/vite.config.ts
  * @Description:
  */
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 import Components from 'unplugin-vue-components/vite';
@@ -15,46 +15,50 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 import * as path from 'path';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  mode: 'production',
+export default ({ mode }) => {
+  const modeConfig = loadEnv(mode, process.cwd());
 
-  plugins: [
-    vue(),
-    vueJsx(),
-    Components({
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: false // css in js
-        })
+  return defineConfig({
+    base: modeConfig.VITE_MODE == 'mock' ? './' : '/',
+    build: {
+      outDir: modeConfig.VITE_MODE == 'mock' ? 'docs' : 'dist'
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false // css in js
+          })
+        ]
+      })
+    ],
+    server: {
+      hmr: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000/',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: path.resolve('./src')
+        },
+        {
+          find: '@layout',
+          replacement: path.resolve('./src/layout/')
+        },
+        {
+          find: '@views',
+          replacement: path.resolve('./src/views/')
+        }
       ]
-    })
-  ],
-  server: {
-    hmr: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
     }
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve('./src')
-      },
-      {
-        find: '@layout',
-        replacement: path.resolve('./src/layout/')
-      },
-      {
-        find: '@views',
-        replacement: path.resolve('./src/views/')
-      }
-    ]
-  }
-});
+  });
+};
