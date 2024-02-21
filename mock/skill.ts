69117eacb7a8bfe05040dee9fc8dbcb464bb3159
PengChaoQun <1152684231@qq.com>
2024-02-21 15:41:00 +0800

    ✨ feat: 增加笔记统计mock接口

--- mock/skill.ts
+++ mock/skill.ts
@@ -2,7 +2,7 @@
  * @Author: PengChaoQun 1152684231@qq.com
  * @Date: 2024-01-30 19:14:17
  * @LastEditors: PengChaoQun 1152684231@qq.com
- * @LastEditTime: 2024-02-18 16:21:25
+ * @LastEditTime: 2024-02-21 15:37:12
  * @FilePath: /experience-book-vue3/mock/skill.ts
  * @Description:
  */
@@ -15,86 +15,6 @@ import { SuccessModel } from '../src/api/config/model';
 function regUrl(url: string) {
   return RegExp(url + '.*');
 }
-
-// /**
-//  * 获取导航的技能列表
-//  */
-// Mock.mock('/api/skill/options', 'get', () => {
-//   const data = Mock.mock([
-//     {
-//       id: '@id',
-//       name: 'UI设计'
-//     },
-//     {
-//       id: '@id',
-//       name: '前端开发'
-//     },
-//     {
-//       id: '@id',
-//       name: '文案写作'
-//     },
-//     {
-//       id: '@id',
-//       name: '运营技巧'
-//     }
-//   ]);
-
-//   return new SuccessModel({
-//     code: 1,
-//     msg: 'success',
-//     data: data
-//   });
-// });
-
-// /**
-//  * 获取技能列表页的数据，包含了经验、任务信息等
-//  */
-// Mock.mock('/api/skill/list', 'get', () => {
-//   const mockData = Mock.mock({
-//     'data|7': [
-//       {
-//         id: '@id',
-//         name: '@cword@cword@cword@cword',
-//         'levelName|1': ['新手', '高级新手', '胜任者', '精通者', '专家'],
-//         // 当前熟练度等级级别，新手1 ，新手2什么的
-//         'level|1-5': 1,
-//         'currentLevelExp|0-100': 1,
-//         'color|1': ['green', 'blue', 'red', 'purple', 'black'],
-//         range: [100, 199]
-//       }
-//     ]
-//   });
-
-//   return new SuccessModel({
-//     code: 1,
-//     msg: 'success',
-//     data: mockData.data
-//   });
-// });
-
-// /**
-//  * 获取技能里的笔记列表
-//  */
-// Mock.mock(regUrl('/api/skill/note-list'), 'get', options => {
-//   const mockData = Mock.mock({
-//     'expTotal|100-420': 1,
-//     'noteList|10-20': [
-//       {
-//         id: '@id',
-//         title: '@cword@cword@cword@cword@cword',
-//         summary: '@cparagraph',
-//         'exp|1-5': 1,
-//         createDateTime: '@datetime'
-//       }
-//     ]
-//   });
-
-//   return new SuccessModel({
-//     code: 1,
-//     msg: 'success',
-//     data: mockData
-//   });
-// });
 
 const baseData = [
   {
@@ -282,6 +202,50 @@ export default [
           color: 'blue',
           range: [800, 999],
           levelExpCap: 199
+        }
+      ]);
+
+      return new SuccessModel({
+        code: 1,
+        msg: 'success',
+        data: data
+      });
+    }
+  },
+  {
+    url: '/mock/skill/note/statistics',
+    method: 'get',
+    response: () => {
+      const data = Mock.mock([
+        {
+          name: '前端',
+          total: 3,
+          hasExp: 'exp>0的笔记数'
+        },
+        {
+          name: '前端',
+          total: 1,
+          hasExp: 'exp=0的笔记数'
+        },
+        {
+          name: 'AE',
+          total: 1,
+          hasExp: 'exp>0的笔记数'
+        },
+        {
+          name: '平面设计',
+          total: 2,
+          hasExp: 'exp>0的笔记数'
+        },
+        {
+          name: '后端',
+          total: 1,
+          hasExp: 'exp>0的笔记数'
+        },
+        {
+          name: 'PR剪辑',
+          total: 1,
+          hasExp: 'exp>0的笔记数'
         }
       ]);
 
